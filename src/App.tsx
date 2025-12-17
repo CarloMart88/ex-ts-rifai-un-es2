bcce902524dd2f76a5a9fe64b9cdb6df66cdb0f1
CarloMart88 <carlo.martino1988@gmail.com>
2025-12-17 18:52:25 +0100

    feat start milestone1

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/**📌 Milestone 1: Mostrare la lista dei prodotti
 
+
+1. Parti dall’array products fornito: */
+   const products = [
+   { name: 'Mela', price: 0.5 },
+   { name: 'Pane', price: 1.2 },
+   { name: 'Latte', price: 1.0 },
+   { name: 'Pasta', price: 0.7 },
+   ]
+/*
+   Crea un componente che mostra la lista dei prodotti.
+   Per ogni prodotto, mostra:
+   Nome
+   Prezzo
+
+Obiettivo: Vedere un elenco leggibile di tutti i prodotti con nome e prezzo.
+*/
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
