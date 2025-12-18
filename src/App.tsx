552c2593a270e847beabbebb64a8c97fe8da3f0c
CarloMart88 <carlo.martino1988@gmail.com>
2025-12-18 11:56:38 +0100

    feat work on AddaddedProducts html part

--- src/App.tsx
+++ src/App.tsx
@@ -88,6 +88,20 @@ function App() {
       </ul>
     </div>
   </div>
+  <div className="row justify-content-center">
+     <div className="col-12 col-md-8 my-5">
+      <h3>Il tuo carrello</h3>
+      {addedProducts.length > 0 && addedProducts ?  (addedProducts.map(a => {
+        return (
+          <div className="col-5 my-5">
+            <p>Prodotto: {a.name}</p>
+            <p>Prezzo: {a.price}£</p>
+            <p>Quantità: {a.quantity}</p>
+          </div>
+        )
+      }) ) : (<h3>il tuo carrello è vuoto</h3>)}
+     </div>
+  </div>
 </div>
 
   )
